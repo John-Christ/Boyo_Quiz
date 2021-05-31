bb53d504336aadfd5e2a5959b1084ba05961e91d
john christ bouma <52007598+John-Christ@users.noreply.github.com>
2021-05-31 20:30:33 +0200

    Update

--- docs/payment.php
+++ docs/payment.php
@@ -1,19 +1,24 @@
-<?php 
-if(isset($_POST['submit'])){
-    $to = "johnnybrozer@gmail.com"; // this is your Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $subject = "Form submission";
-    $subject2 = "Copy of your form submission";
-    $message = $first_name . " " . $last_name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
+<?php
+  $name = $_POST['name'];
+  $visitor_email = $_POST['email'];
+  $message = $_POST['message'];
+?>
+<?php
+	$email_from = 'johnnybrozer@gmail.com';
 
-    $headers = "From:" . $from;
-    $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
-    mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
-    echo "Mail Sent. Thank you " . $first_name . ", we will contact you shortly.";
-    // You can also use header('Location: thank_you.php'); to redirect to another page.
-    }
+	$email_subject = "New Form submission";
+
+	$email_body = "You have received a new message from the user $name.\n".
+                            "Here is the message:\n $message".
+?>
+<?php
+
+$to = "johnnybrozer@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
 ?>
