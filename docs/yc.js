644afa707c4365f15fe0ecbf7a3851d3fe7ef73c
john christ bouma <52007598+John-Christ@users.noreply.github.com>
2021-07-26 22:09:48 +0200

    Create

---
+++ docs/yc.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+
+// Anonymous test key. Replace with your key.
+const SECRET_KEY = 'sk_live_d2acec067LM2OJ9b1d5418b9b7d9'
+
+axios.post(
+  'https://online.yoco.com/v1/charges/',
+  {
+    token: 'tok_0PDoyIR0W405X5u4POhpQzU95V',
+    amountInCents: 500,
+    currency: 'ZAR',
+  },
+  {
+    headers: {
+      'X-Auth-Secret-Key': SECRET_KEY,
+    },
+  },
+)
+.then(res => {
+  // res.status will contain the HTTP status code
+  // res.data will contain the response body
+})
+.catch(error => {
+  // handle errors
+})
